b2359b70a1693ae7cb34841b986ed135740664c2
matteo2911 <72656567+matteo2911@users.noreply.github.com>
2025-06-05 10:57:05 +0200

    refactoring

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ d3.json("data.json").then(data => {
   const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
   function getCoords(d, state) {
-    if(state === 0) return [xScale(d.v1), yScale(d.v2)];
-    if(state === 1) return [xScale(d.v3), yScale(d.v4)];
-    if(state === 2) return [xScale(d.v5), yScale(d.v6)];
+    if (state === 0) return [xScale(d.v1), yScale(d.v2)];
+    if (state === 1) return [xScale(d.v3), yScale(d.v4)];
+    if (state === 2) return [xScale(d.v5), yScale(d.v6)];
   }
 
   const symbols = svg.selectAll("path")
@@ -33,7 +33,7 @@ d3.json("data.json").then(data => {
     .enter()
     .append("path")
     .attr("d", d3.symbol().type(d3.symbolStar).size(800))
-    .attr("fill", (d,i) => colorScale(i))
+    .attr("fill", (d, i) => colorScale(i))
     .attr("stroke", "black")
     .attr("stroke-width", 1)
     .attr("transform", d => {
@@ -41,86 +41,25 @@ d3.json("data.json").then(data => {
       return `translate(${x},${y})`;
     });
 
-  const trailsData = data.map(() => []);
-
-  function updateTrails() {
-    trailsData.forEach((trail, i) => {
-      if (trail.length > 10) trail.shift();
-
-      const trailSelection = svg.selectAll(`circle.trail-${i}`)
-        .data(trail, (d, idx) => idx);
-
-      trailSelection.enter()
-        .append("circle")
-        .attr("class", `trail trail-${i}`)
-        .attr("r", 5)
-        .attr("fill", "gold")
-        .attr("opacity", 0)
-        .attr("cx", d => d[0])
-        .attr("cy", d => d[1])
-        .merge(trailSelection)
-        .attr("cx", d => d[0])
-        .attr("cy", d => d[1])
-        .attr("opacity", (d, idx) => (idx + 1) / trail.length * 0.8);
-
-      trailSelection.exit().remove();
-    });
-  }
-
-  function fadeOutTrail(i) {
-    // seleziona i cerchi di questa scia
-    const circles = svg.selectAll(`circle.trail-${i}`);
-
-    circles.transition()
-      .duration(800)      // durata dissolvenza
-      .attr("opacity", 0)
-      .remove();
-
-    // svuota l’array della scia
-    trailsData[i] = [];
-  }
-
   function animateToState(state) {
-  let finished = 0;
-  const total = symbols.size();
+    symbols.transition()
+      .duration(1000)
+      .ease(d3.easeLinear)
+      .attrTween("transform", function(d) {
+        const [x0, y0] = this.__currentPos || getCoords(d, clickState);
+        const [x1, y1] = getCoords(d, state);
 
-  symbols.transition()
-    .duration(1000)
-    .ease(d3.easeLinear)
-    .attrTween("transform", function(d, i) {
-      const [x0, y0] = this.__currentPos || getCoords(d, clickState);
-      const [x1, y1] = getCoords(d, state);
+        this.__currentPos = [x1, y1];
 
-      this.__currentPos = [x1, y1];
-      trailsData[i] = [];
+        return function(t) {
+          const x = x0 + (x1 - x0) * t;
+          const y = y0 + (y1 - y0) * t;
+          return `translate(${x},${y})`;
+        };
+      });
+  }
 
-      return function(t) {
-        const x = x0 + (x1 - x0) * t;
-        const y = y0 + (y1 - y0) * t;
-
-        trailsData[i].push([x, y]);
-        if (trailsData[i].length > 10) trailsData[i].shift();
-
-        updateTrails();
-
-        return `translate(${x},${y})`;
-      };
-    })
-    .on("end", function(d, i) {
-      finished++;
-      if (finished === total) {
-        
-        for(let j = 0; j < total; j++) trailsData[j] = [];
-        svg.selectAll("circle.trail")
-          .transition()
-          .duration(800)
-          .attr("opacity", 0)
-          .remove();
-      }
-    });
-}
-
-  symbols.each(function(d, i) {
+  symbols.each(function(d) {
     this.__currentPos = getCoords(d, 0);
   });
 
